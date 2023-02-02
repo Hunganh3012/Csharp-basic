492f648b55be233f025d3cfeaf2d78e45caec73e
Hunganh3012 <nguyenhunganh072017@gmail.com>
2023-02-02 11:59:05 +0700

    update  kieudulieu

--- ChuoiString/Program.cs
+++ ChuoiString/Program.cs
@@ -17,13 +17,19 @@ namespace ChuoiString
         {
         string thongbao = "      Nguyen Hung Anh";
             Console.WriteLine(thongbao);
-            thongbao = thongbao.Trim();
+            thongbao = thongbao.TrimEnd(); // Xóa bỏ các kí tự cho trước trong key hoặc chars ở đầu và cuối string
+                                        //và trả về string sau khi xóa. Nếu không cung cấp tham số thì hàm xóa bỏ các kí tự khoảng trắng.
+            Console.WriteLine(thongbao); // Nguyen Hung Anh .. đã xóa khoảng trăng
+            
+            thongbao = thongbao.Trim().Insert(0, "  Lớp:12"); // Tương tự, Hàm Insert Chèn value vào vị trí startPos của string hiện tại
+                                                              // và trả về string mới sau khi chèn. Insert( StartPos, value).
+            Console.WriteLine(thongbao); // Lớp:12Nguyen Hung Anh
+            thongbao = thongbao.Replace("Nguyen", "Tran"); //	Trả về một string sau khi thay thế tất cả các kí tự
+                                                             //	hoặc string old bởi kí tự hoặc string trong tham số new. Replace(old,new).
             Console.WriteLine(thongbao);
-            thongbao = thongbao.Trim().Insert(0, "  Lớp:12");
-            Console.WriteLine(thongbao);
-            thongbao = thongbao.Replace("Nguye", "Tran");
-            Console.WriteLine(thongbao);
+         
+        }
 
-        } 
+         
     }
 }
---
+++ Kieudulieu/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Kieudulieu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------Ép kiểu Parse--------------
+            //Parse sẽ chuyển kiểu chuỗi thành 1 kiểu khác . và nếu không đúng định dạng
+            // sẽ trả về thông báo ngoại lệ(exception)
+            int a = Int32.Parse("123"); //chuyễn chuỗi 123 thành kiểu số nguyen
+            //Console.WriteLine(a);
+            //float b = float.Parse("Hunganh");
+            //Console.WriteLine(b); // kiểu sai định dạng sẽ bị exception
+
+
+
+            //------------------Ép kiểu TryParse---------------
+            //Tương tự như Parse nhưng sẽ không xuất ra ngoại lệ. nếu sai định dạng sẽ in ra false hoặc 0
+            //float c;
+            //float.TryParse("12.7", out c);
+            //Console.WriteLine(c);
+
+            //-------------------------Ép kiểu Convert----------------
+            //int d = Convert.ToInt32("123");
+           // Console.WriteLine(d); // chuyển chuỗi sang số
+            //bool e = Convert.ToBoolean(27);
+            //Console.WriteLine(e); //chuyễn số sang bool
+
+
+            //--ví dụ----------
+
+            //Console.WriteLine("Nhap tuoi: ");
+            //int age = int.Parse(Console.ReadLine());
+
+
+
+            //----------------------------------------------Các TOÁN TỬ--------------------
+            //dễ
+            //------------------------------------Cá phép tính toán hàm math-------
+            int x = 10;
+            int y = 4;
+
+            Console.WriteLine( Math.Pow(x, y) - Math.Sqrt(x)  );
+             
+
+        }
+    }
+}
